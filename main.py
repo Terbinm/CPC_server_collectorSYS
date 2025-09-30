e0a6af7147cb99db91686981f8b645e53d7e21a3
Terbinm <a0978672@gmail.com>
2025-09-30 23:51:47 +0800

    stable_v4.1.1 update ui

--- main.py
+++ main.py
@@ -53,6 +53,6 @@ if __name__ == '__main__':
     logger.info("啟動應用程序")
     socketio.run(app,
                  host='0.0.0.0',
-                 port=5001,
+                 port=5000,
                  debug=True,
                  allow_unsafe_werkzeug=True)
